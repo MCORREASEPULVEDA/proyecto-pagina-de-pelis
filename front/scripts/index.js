c7ea962fa66b37371c274f2f599472cd7d00c431
ManuelaCorrea <correamanuela13@gmail.com>
2024-04-21 22:23:24 -0500

    M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,28 +1,25 @@
-const filmSection = documento.getElementById('films')
+const filmsSection = document.getElementById('films');
 
-const renderFilms = tempData. map((movie)=>{
-    const movieElement = document.creadteElement('article')
-    const containerMovie = document.createElement('div')
+    const renderFilms = (movie) => {
+        const movieElement = document.createElement('article');
+        const containerMovie = document.createElement('div');
 
-    movieElement.classList.add('movie')
-    containerMovie.classList.add('divMovie')
-    
-    movieElement.innerHTML = `<img src="${movie.poster}"
-    alt=${movie.tittle}>`
+        movieElement.classList.add('movie');
+        containerMovie.classList.add('divMovie');
 
-    containerMovie.innerHTML =`
-    <h3>${movie.tittle} - (${movie.year}) </h3>
-    <p><strong>Director: </strong> ${movie.director}</p>
-    <p><strong>Duración: </strong> ${movie.duration}</p>
-    <p><strong>Género: </strong> ${movie.genre.join(', ')}</p>
-    <p><strong>Rate: </strong> ${movie.rate}</p>`
+        movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`;
 
+        containerMovie.innerHTML = `
+            <h3>${movie.title} - (${movie.year})</h3>
+            <p><strong>Director:</strong> ${movie.director}</p>
+            <p><strong>Duración:</strong> ${movie.duration}</p>
+            <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+            <p><strong>Rate:</strong> ${movie.rate}</p>
+        `;
 
-movieElement.appendChild(containerMovie)
-filmSection.appendChild(movieElement)
-
-})
-
-$.get('https://students-api.up.railway.app/movies', (data)=> data.map(renderFilms)).fail(()=>alert('Error al traer las peliculas'))
+        movieElement.appendChild(containerMovie);
+        filmsSection.appendChild(movieElement);
+    };
 
 
+    $.get('https://students-api.up.railway.app/movies', (data) => data.map(renderFilms)).fail(()=>alert('Error al traer las peliculas'));
