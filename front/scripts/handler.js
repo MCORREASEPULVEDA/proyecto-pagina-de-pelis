d9bf91d719d341a2b7f14ad9999dd5b41f61047c
ManuelaCorrea <correamanuela13@gmail.com>
2024-04-22 20:34:17 -0500

    Front

---
+++ front/scripts/handler.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+const renderFilms = require('./renderFilms')
+
+const getFilms = () => {
+    axios.get('https://students-api.up.railway.app/movies')
+    .then((response) => {
+        console.log(response)
+        response.data.map(renderFilms)
+    }).catch((error)=> alert(error.message))
+}
+
+module.exports = getFilms
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,25 +1,3 @@
-const filmsSection = document.getElementById('films');
+const getFilms = require ('./handler')
 
-    const renderFilms = (movie) => {
-        const movieElement = document.createElement('article');
-        const containerMovie = document.createElement('div');
-
-        movieElement.classList.add('movie');
-        containerMovie.classList.add('divMovie');
-
-        movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`;
-
-        containerMovie.innerHTML = `
-            <h3>${movie.title} - (${movie.year})</h3>
-            <p><strong>Director:</strong> ${movie.director}</p>
-            <p><strong>Duración:</strong> ${movie.duration}</p>
-            <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
-            <p><strong>Rate:</strong> ${movie.rate}</p>
-        `;
-
-        movieElement.appendChild(containerMovie);
-        filmsSection.appendChild(movieElement);
-    };
-
-
-    $.get('https://students-api.up.railway.app/movies', (data) => data.map(renderFilms)).fail(()=>alert('Error al traer las peliculas'));
+getFilms()
